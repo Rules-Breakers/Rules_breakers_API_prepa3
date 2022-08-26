ef7ec85fd583a30826b561735484e2af133fbf9f
ylan <hei.nalisoa@gmail.com>
2022-08-26 07:50:25 +0300

    feat : create spring app

---
+++ src/main/java/com/hackathon3/Hackathon3Application.java
@@ -0,0 +1,13 @@
+package com.hackathon3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackathon3/Hackathon3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
