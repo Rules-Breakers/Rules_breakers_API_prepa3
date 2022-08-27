5ecf8ea2a8fdde3916809c45c7a2c14d8d539c3a
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-27 18:00:45 +0300

    chore:final commit "

--- src/main/java/com/hackathon3/service/BookService.java
+++ src/main/java/com/hackathon3/service/BookService.java
@@ -2,7 +2,6 @@ package com.hackathon3.service;
 
 import com.hackathon3.endpoint.mapper.restBorrow.RestBorrow;
 import com.hackathon3.model.Book;
-import com.hackathon3.model.Borrow;
 import com.hackathon3.repository.BookRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -15,30 +14,34 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class BookService {
-    private final BookRepo bookRepo;
-    public List<Book> getAll(int page, int page_size) {
-        Pageable pageable = PageRequest.of(page,page_size);
-        return  bookRepo.findAll(pageable).toList();
-    }
+  private final BookRepo bookRepo;
 
-    public Optional<Book> getBookById(Long id){
-        return bookRepo.findById(id);
-    }
-    public List<Book> getBookStatus (int page , int page_size , boolean available) {
-        Pageable pageable = PageRequest.of(page,page_size);
-        return bookRepo.findBookByAvailable(pageable , available);
-    }
-    public void deleteBook (Long id){
-        boolean exist = bookRepo.findById(id).isPresent();
-        if(exist) bookRepo.deleteById(id);
-    }
+  public List<Book> getAll(int page, int page_size) {
+    Pageable pageable = PageRequest.of(page, page_size);
+    return bookRepo.findAll(pageable).toList();
+  }
 
-    public Book updateBooks(RestBorrow borrow){
-        Optional<Book> book = bookRepo.findById(borrow.getId_book());
-        book.get().setAvailable(false);
-        return bookRepo.save(book.get());
-    }
-    public List<Book> saveAllBook(List<Book> toWrite) {
-        return bookRepo.saveAll(toWrite);
-    }
+  public Optional<Book> getBookById(Long id) {
+    return bookRepo.findById(id);
+  }
+
+  public List<Book> getBookStatus(int page, int page_size, boolean available) {
+    Pageable pageable = PageRequest.of(page, page_size);
+    return bookRepo.findBookByAvailable(pageable, available);
+  }
+
+  public void deleteBook(Long id) {
+    boolean exist = bookRepo.findById(id).isPresent();
+    if (exist) bookRepo.deleteById(id);
+  }
+
+  public Book updateBooks(RestBorrow borrow) {
+    Optional<Book> book = bookRepo.findById(borrow.getId_book());
+    book.get().setAvailable(true);
+    return bookRepo.save(book.get());
+  }
+
+  public List<Book> saveAllBook(List<Book> toWrite) {
+    return bookRepo.saveAll(toWrite);
+  }
 }
